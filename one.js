ac6e9e67789590c84d8696387c7b809dcbbb0eda
Jay Kumar jangid <106320129+jaykumar2004@users.noreply.github.com>
2024-10-29 15:05:34 +0530

    Hoisting in JS

---
+++ one.js
@@ -0,0 +1,45 @@
+
+var x = 7;
+
+function getName (){
+    console.log("Namaste JavaScript");
+    
+}
+
+getName()
+console.log(x);
+console.log(getName);
+
+
+
+// getName()
+// console.log(x);
+// console.log(getName);
+
+// var x = 7;
+
+// function getName2 (){ //function
+
+// }
+
+// var getName = () =>{  //undefined
+//     console.log("Java Script");
+// }
+
+
+
+
+
+// getName()
+// console.log(x);
+// console.log(getName);
+
+// var x = 7;
+
+// var getName2 = function(){ //undefined
+
+// }
+
+// var getName = () =>{ //undefined
+//     console.log("Java Script");
+// }
